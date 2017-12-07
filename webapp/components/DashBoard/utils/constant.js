00f68689cb68c12c3d55de427c64c9d263c584e6
salman18693 <salmankhan@innotical.com>
2017-12-07 12:47:10 +0530

    change basepath

--- webapp/components/DashBoard/utils/constant.js
+++ webapp/components/DashBoard/utils/constant.js
@@ -1,2 +1,3 @@
-export const basepath='https://nodetesting.1thing.io/';
+export const basePath='https://nodeapi.1thing.io/'; //production
+// export const basepath='https://nodetesting.1thing.io/'; //staging
 export const channelpath='http://workspace.1thing.io/onething/channels/';
